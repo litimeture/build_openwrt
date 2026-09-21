c4a15af1f72b871813e4f661349bdfd4bf772373
litimeture <3463936681@qq.com>
2026-09-21 19:13:26 +0800

    Clone UA3F repository in diy-part1.sh
    Added a command to clone the UA3F repository into the package directory.

--- config/Lienol-master/diy-part1.sh
+++ config/Lienol-master/diy-part1.sh
@@ -1,14 +1,1 @@
-#!/bin/bash
-#========================================================================================================================
-# https://github.com/ophub/amlogic-s9xxx-openwrt
-# Description: Automatically Build OpenWrt
-# Function: Diy script (Before Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
-# Source code repository: https://github.com/openwrt/openwrt / Branch: main
-#========================================================================================================================
-
-# Add a feed source
-# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
-# other
-# rm -rf package/utils/{ucode,fbtest}
-
+git clone https://github.com/SunBK201/UA3F.git package/UA3F
